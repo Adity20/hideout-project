eef3214aeacc3db00afd918bb7123b538d4eae71
Aditya Sharma <sharma.aditya9164@gmail.com>
2024-04-02 14:12:06 +0530

    SOME CHANGES

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from "./pages/SignIn";
 import SignOut from "./pages/SignOut";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 
 export default function App() {
     return <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
